62c905465362e4867f90e005d3652c5cb685baf6
Ivan Art <tobedrive@yandex.ru>
2024-05-17 10:13:33 +0300

    init

---
+++ main/app.py
@@ -0,0 +1,41 @@
+import asyncio
+import os
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.enums import ParseMode
+from aiogram.client.bot import DefaultBotProperties
+
+from dotenv import find_dotenv, load_dotenv
+load_dotenv(find_dotenv())
+
+from database.engine import create_db, drop_db, session_maker
+
+from handlers.for_user import for_user_router
+from common.bot_menu_cmds import bot_menu
+from middlewares.db import DataBaseSession
+
+bot = Bot(token=os.getenv("TOKEN"), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher()
+
+dp.include_router(for_user_router)
+
+
+async def on_startup(bot):
+    await drop_db()
+
+    await create_db()
+
+
+async def main():
+    dp.startup.register(on_startup)
+    dp.update.middleware(DataBaseSession(session_pool=session_maker))
+    # await bot.set_my_commands(scope=types.BotCommandScopeAllPrivateChats) #команда для удаления кнопки меню
+    await bot.set_my_commands(commands=bot_menu, scope=types.BotCommandScopeAllPrivateChats())
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Exit")
