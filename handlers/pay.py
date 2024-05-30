178cd8bcc87aad34d9f97959dc6f3a3b71ea9698
Ivan Art <tobedrive@yandex.ru>
2024-05-30 18:09:59 +0300

    Создал файл pay.py для оплаты под Юкассу

--- handlers/pay.py
+++ handlers/pay.py
@@ -1,4 +1,28 @@
 from aiogram import Bot
-from aiogram.types import Message, LabeledPrice
+from aiogram.types import Message, LabeledPrice, PreCheckoutQuery
+
+
+async def order(message: Message, bot: Bot):
+    await bot.send_invoice(
+        chat_id=message.chat.id,
+        title="Поиск запчастей для детских колясок",
+        description="Рассылка запроса на подбор комплектующих для детской коляски",
+        provider_token=os.getenv("PROVIDER_TOKEN"),
+        currency="RUB",
+        prices=[
+            LabeledPrice(
+                label="Оплата услуги",
+                amount=250
+            ),
+            LabeledPrice(
+                label="Скидка",
+                amount=-151
+            )
+        ],
+        photo_url=FSInputFile("./mediafile_for_bot/1_zapchasti.jpg"),
+        need_shipping_address=False,
+        is_flexible=False,
+        request_timeout=15
+    )
 
 
